17c23dab06567614d5e4dda0d8c0e64d9f7f583b
Pino0202 <pinochi0202@gmail.com>
2023-09-28 00:42:42 +0900

    step 1

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
-import pytesseract
+import sys
+import cv2
+import pyocr
+import numpy as np
 from PIL import Image
 
-# 画像を読み込む
-image = Image.open('ss.png')
+image = "ss.png"
+name = "test"
 
-# Tesseractを使用してテキストを抽出
-text = pytesseract.image_to_string(image)
+# original
+img = cv2.imread(image)
+cv2.imwrite(f"1_{name}_original.png", img)
 
-# 抽出したテキストを表示
-print(text)
+# gray
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imwrite(f"2_{name}_gray.png", img)
+
+# threshold
+th = 140
+img = cv2.threshold(img, th, 255, cv2.THRESH_BINARY)[1]
+cv2.imwrite(f"3_{name}_threshold_{th}.png", img)
+
+# bitwise
+img = cv2.bitwise_not(img)
+cv2.imwrite(f"4_{name}_bitwise.png", img)
+
+cv2.imwrite("target.png", img)
+
+tools = pyocr.get_available_tools()
+if len(tools) == 0:
+    print("No OCR tool found")
+    sys.exit(1)
+tool = tools[0]
+res = tool.image_to_string(Image.open("target.png"), lang="eng")
+
+print(res)
